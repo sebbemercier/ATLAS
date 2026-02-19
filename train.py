9d91ef48b3a03fcca55e5bd713d8a0daf52ffb80
Sebbe Mercier <sebbemercier@gmail.com>
2026-02-19 17:26:51 +0100

    ajout requirements

--- train.py
+++ train.py
@@ -5,7 +5,12 @@ from datasets import load_dataset
 import os
 
 def train_slm(model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0"):
-    device = "mps" if torch.backends.mps.is_available() else "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    else:
+        device = "cpu"
     print(f"🚀 Device : {device.upper()}")
     
     tokenizer = AutoTokenizer.from_pretrained(model_id)
@@ -35,7 +40,6 @@ def train_slm(model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0"):
         num_train_epochs=1,
         learning_rate=1e-4,
         logging_steps=10,
-        use_mps_device=True,
         save_strategy="no",
         report_to="none"
     )
